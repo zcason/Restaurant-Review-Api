60eb94848bed9c6ae65238cf655874f76a9871c7
zcason <zuri.cason@gmail.com>
2021-01-21 15:09:26 -0600

    database up and running

---
+++ seeds/seed.restaurants.sql
@@ -0,0 +1,7 @@
+INSERT INTO restaurants (name, location, price_range)
+VALUES
+('Chick-fil-A', 'Atlanta', 2),
+('Shake Shack', 'New York', 2),
+('Uchi', 'Austin', 5),
+('Cava', 'Austin', 3),
+('Flower Child', 'Atlanta', 4);
