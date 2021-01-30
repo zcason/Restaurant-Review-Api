3f16467c9a23c002e5d9b859a5b22e568ccb1564
zcason <zuri.cason@gmail.com>
2021-01-29 20:34:35 -0600

    change node env

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'development')
+    "connectionString": (process.env.NODE_ENV === 'production')
         ? process.env.DATABASE_URL
         : process.env.TEST_DATABASE_URL,
 }
