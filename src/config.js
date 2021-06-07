32688f6078669524c72be73d73c8523ce6ea825c
zcason <zuri.cason@gmail.com>
2021-06-07 13:29:33 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'production',
+    NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/test_res_review'
 };
