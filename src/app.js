ddf5afdb121b047b851f00fd944b470daff36277
zcason <zuri.cason@gmail.com>
2021-06-07 13:32:48 -0500

    console error to debbug server issue

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use('/api/restaurants', homeRouter);
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
+        console.log(error)
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
