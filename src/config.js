fbbd0ee98eee431cfc33a17217f3bafb6a0e0bd5
zcason <zuri.cason@gmail.com>
2021-06-08 16:27:59 -0500

    upgrade config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? '?ssl=true' : '') || 'postgresql://dunder_mifflin@localhost/res_review',
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/test_res_review'
 };
