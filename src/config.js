a4c084327a462382495e9af51219ff88a167ae99
zcason <zuri.cason@gmail.com>
2021-06-09 13:30:15 -0500

    updated database_url in config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    DATABASE_URL: process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? '?ssl=true' : '') || 'postgresql://dunder_mifflin@localhost/res_review',
+    DATABASE_URL: process.env.DATABASE_URL,
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/test_res_review'
 };
